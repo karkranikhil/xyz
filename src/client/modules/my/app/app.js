95825729f913bd64eb77f721f7ab1aad3f129f8c
Nikhil Karkra <nikuideveloper@gmail.com>
2021-05-25 22:13:59 +1000

    bootstrap 5

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,21 +1,4 @@
 import {LightningElement} from 'lwc'
 export default class App extends LightningElement{
-     name="sftroop"
-     technologies = ['LWC OSS', 'Bootstrap', 'ExpressJs', 'Nodemailer', 'Heroku']
-     
-     greetingmsg="Hello i am greeting msg from app component"
-     changeName(){
-         this.name="LWC OSS"
-     }
-
-     get addNumber(){
-         return 1+2
-     }
-
-
-     nameChange(event){
-        this.name = event.detail
-     }
-
-
+  
 }
