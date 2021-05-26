80538a5104d8a09777eb1082e56c606aea528636
Nikhil Karkra <nikuideveloper@gmail.com>
2021-05-26 22:19:19 +1000

    banner component

---
+++ src/client/modules/my/banner/banner.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+export default class Banner extends LightningElement {
+    heading='Salesforce Implementation and Consulting Services'
+    description='We have a fantastic team of Salesforce accredited experts and advisors who are dedicated to delivering nothing but the best.'
+}
