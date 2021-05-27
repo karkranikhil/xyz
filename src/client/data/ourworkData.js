2e6009aa38b8c6c0ea68779011d33d66c2922073
Nikhil Karkra <nikuideveloper@gmail.com>
2021-05-27 22:53:11 +1000

    our work component

---
+++ src/client/data/ourworkData.js
@@ -0,0 +1,18 @@
+const OUR_WORK_DATA = {
+    sectionHeading:'Our Work',
+    points:[
+        {label:'Integration of numerous CRM solutions and modules', icon:'./resources/Images/Work_section/icon1.png'},
+        {label:'Real-time data synchronization', icon:'./resources/Images/Work_section/icon2.png'},
+        {label:'Lightning-based pixel-perfect designs using Lightning UI', icon:'./resources/Images/Work_section/icon3.png'},
+        {label:'Customization and Security in Salesforce', icon:'./resources/Images/Work_section/icon4.png'},
+        {label:'System evolution services with constant user help desk', icon:'./resources/Images/Work_section/icon5.png'},
+        {label:'Deploy Lightning apps for enterprises and startups', icon:'./resources/Images/Work_section/icon2.png'}
+    ],
+    images:[
+        './resources/Images/Work_section/work1.PNG',
+        './resources/Images/Work_section/work2.PNG',
+        './resources/Images/Work_section/work3.PNG',
+        './resources/Images/Work_section/work4.PNG'
+    ]
+}
+export default OUR_WORK_DATA
