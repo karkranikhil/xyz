e5e2c4056d14627c302b98eef814f7b3dd0b5346
nikhil karkra <karkra.nikhil@gmail.com>
2022-05-17 20:55:56 +1000

    Update app.js

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,4 +1,10 @@
 import {LightningElement} from 'lwc'
 export default class App extends LightningElement{
-  
+  cscroll(event){
+    const tagname = event.detail
+    const elem = this.template.querySelector(tagname)
+    if(elem){
+      elem.scrollIntoView()
+    }
+  }
 }
